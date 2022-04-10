dd047ce3a3cff704520ebe43d558474974a0054b
snsina <sn@sns-MacBook-Pro.local>
2022-04-10 11:10:11 -0400

    blah

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# tag=$1
-# sha=$2
-# token=$3
-# export GITHUB_TOKEN=$token
+tag=$1
+sha=$2
+token=$3
+export GITHUB_TOKEN=$token
 
-# git pull origin
-# git tag -f $tag $sha
-# git push origin -f $tag
-echo  hello
+git pull origin
+git tag -f $tag $sha
+git push origin -f $tag
